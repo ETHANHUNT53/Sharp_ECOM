385dd285af4f43cdef72adbcc575c86deca007a2
Shaurya Jaiswal <shreyjaiswal411@gmail.com>
2025-07-18 17:02:11 +0530

    added remove item button

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -4,6 +4,7 @@ import {
   clearCart,
   decreaseQuantity,
   increaseQuantity,
+  removeFromCart,
   updateTotalPrice,
 } from "../utils/cartSlice";
 import { useNavigate } from "react-router-dom";
@@ -37,11 +38,11 @@ const CartPage = () => {
     dispatch(clearCart());
   };
   return (
-    <section className=" relative  after:contents-[''] after:absolute after:z-0 after:h-full xl:after:w-1/3 after:top-0 after:right-0 after:bg-gray-50">
+    <section className=" relative  after:contents-[''] after:absolute after:z-0 after:h-full xl:after:w-1/3 after:top-0 after:right-0 after:bg-gray-50 ">
       <div className="w-full max-w-7xl px-4 md:px-5 lg-6 mx-auto relative z-5">
         <div className="grid grid-cols-12">
           <div className="col-span-12 xl:col-span-8 lg:pr-8 pt-14 pb-8 lg:py-24 w-full max-xl:max-w-3xl max-xl:mx-auto">
-            <div className="flex items-center justify-between pb-8 border-b border-gray-300">
+            <div className="flex items-center justify-between pb-8 border-b border-gray-300 mt-16">
               <h2 className="font-manrope font-bold text-3xl leading-10 text-black">
                 Shopping Cart
               </h2>
@@ -51,14 +52,14 @@ const CartPage = () => {
             </div>
             {items.length == 0 ? (
               <div className="flex-col my-[25%] mx-[20%] h-full">
-                <h2 className="font-manrope font-semibold text-5xl  leading-10 text-black mb-8">
+                <h2 className="font-manrope font-semibold sm:text-5xl md:text-3xl  leading-10 text-black mb-8">
                   {" "}
                   Your Cart is empty!
                 </h2>
 
                 <div>
                   <button
-                    className="rounded-lg w-[35%] bg-blue-800 py-2.5 px-4 text-white text-sm font-semibold text-center mb-8 transition-all duration-500 hover:bg-blue-900 cursor-pointer ml-25"
+                    className="rounded-lg sm:w-[35%] bg-blue-800 py-2.5 px-4 text-white text-sm font-semibold text-center mb-8 transition-all duration-500 hover:bg-blue-900 cursor-pointer sm:ml-25 md:ml-[5%]"
                     onClick={() => navigate("/products")}
                   >
                     Browse Products
@@ -209,6 +210,14 @@ const CartPage = () => {
                         : item.price * item.quantity}
                     </p>
                   </div>
+                  <div className="flex items-center max-[500px]:justify-center  max-md:mt-3 h-full ">
+                    <button
+                      className="p-1 px-2 bg-black text-white rounded-md cursor-pointer hover:bg-gray-800"
+                      onClick={() => dispatch(removeFromCart(item.id))}
+                    >
+                      Remove Item
+                    </button>
+                  </div>
                 </div>
               </div>
             ))}
@@ -245,32 +254,34 @@ const CartPage = () => {
               </div>
             )}
           </div>
-          <div className=" col-span-12 xl:col-span-4 bg-gray-50 w-full max-xl:px-6 max-w-3xl xl:max-w-lg mx-auto lg:pl-8 py-24">
-            <h2 className="font-manrope font-bold text-3xl leading-10 text-black pb-8 border-b border-gray-300">
-              Order Summary
-            </h2>
-            <div className="mt-8">
-              <div className="flex items-center justify-between pb-6">
-                <p className="font-normal text-lg leading-8 text-black">
-                  {items.length} Items
-                </p>
-                <div className="font-medium text-lg leading-8 text-black">
-                  {" "}
-                  {items.map((item) => (
-                    <p>
-                      ₹{" "}
-                      {item.categories
-                        ? item.price[item.selectedCategoryIndex] * item.quantity
-                        : item.price * item.quantity}
-                    </p>
-                  ))}
+          {items.length !== 0 && (
+            <div className=" col-span-12 xl:col-span-4 bg-gray-50 w-full max-xl:px-6 max-w-3xl xl:max-w-lg mx-auto lg:pl-8 py-24 md:mt-16">
+              <h2 className="font-manrope font-bold text-3xl leading-10 text-black pb-8 border-b border-gray-300">
+                Order Summary
+              </h2>
+              <div className="mt-8">
+                <div className="flex items-center justify-between pb-6">
+                  <p className="font-normal text-lg leading-8 text-black">
+                    {items.length} Items
+                  </p>
+                  <div className="font-medium text-lg leading-8 text-black">
+                    {" "}
+                    {items.map((item) => (
+                      <p>
+                        ₹{" "}
+                        {item.categories
+                          ? item.price[item.selectedCategoryIndex] *
+                            item.quantity
+                          : item.price * item.quantity}
+                      </p>
+                    ))}
+                  </div>
                 </div>
-              </div>
-              <form>
-                {/* <label className="flex  items-center mb-1.5 text-gray-600 text-sm font-medium">
+                <form>
+                  {/* <label className="flex  items-center mb-1.5 text-gray-600 text-sm font-medium">
                   Shipping - Shipping Cost
                 </label> */}
-                {/* <label className="flex items-center mb-1.5 text-gray-400 text-sm font-medium">
+                  {/* <label className="flex items-center mb-1.5 text-gray-400 text-sm font-medium">
                   Promo Code
                 </label>
                 <div className="flex pb-4 w-full">
@@ -293,48 +304,49 @@ const CartPage = () => {
                     Apply
                   </button>
                 </div> */}
-                <div className="">
-                  <div className="flex items-center justify-between ">
-                    <p className="font-medium text-md leading-8 text-black">
-                      {items.length} Items
-                    </p>
-                    <p className="font-semibold text-md leading-8 text-indigo-600">
-                      ₹ {price}
-                    </p>
+                  <div className="">
+                    <div className="flex items-center justify-between ">
+                      <p className="font-medium text-md leading-8 text-black">
+                        {items.length} Items
+                      </p>
+                      <p className="font-semibold text-md leading-8 text-indigo-600">
+                        ₹ {price}
+                      </p>
+                    </div>
+                    <div className="flex items-center justify-between ">
+                      <p className="font-medium text-md leading-8 text-black">
+                        {" "}
+                        GST(@18%)
+                      </p>
+                      <p className="font-semibold text-md leading-8 text-indigo-600">
+                        ₹ {gst}
+                      </p>
+                    </div>
+                    <div className="flex items-center justify-between py-5">
+                      <p className="font-medium text-xl leading-8 text-black">
+                        {" "}
+                        Total
+                      </p>
+                      <p className="font-semibold text-xl leading-8 text-indigo-600">
+                        ₹ {finalTotal}
+                      </p>
+                    </div>
                   </div>
-                  <div className="flex items-center justify-between ">
-                    <p className="font-medium text-md leading-8 text-black">
-                      {" "}
-                      GST(@18%)
-                    </p>
-                    <p className="font-semibold text-md leading-8 text-indigo-600">
-                      ₹ {gst}
-                    </p>
-                  </div>
-                  <div className="flex items-center justify-between py-5">
-                    <p className="font-medium text-xl leading-8 text-black">
-                      {" "}
-                      Total
-                    </p>
-                    <p className="font-semibold text-xl leading-8 text-indigo-600">
-                      ₹ {finalTotal}
-                    </p>
-                  </div>
-                </div>
-                <button
-                  className={`w-full text-center  rounded-xl py-3 px-6 font-semibold text-lg text-white transition-all duration-500  ${
-                    items.length == 0
-                      ? "bg-gray-400 cursor-not-allowed"
-                      : "bg-green-700 text-white hover:bg-green-800 cursor-pointer"
-                  }`}
-                  onClick={handleCheckout}
-                  disabled={items.length == 0}
-                >
-                  Proceed to Checkout
-                </button>
-              </form>
+                  <button
+                    className={`w-full text-center  rounded-xl py-3 px-6 font-semibold text-lg text-white transition-all duration-500  ${
+                      items.length == 0
+                        ? "bg-gray-400 cursor-not-allowed"
+                        : "bg-green-700 text-white hover:bg-green-800 cursor-pointer"
+                    }`}
+                    onClick={handleCheckout}
+                    disabled={items.length == 0}
+                  >
+                    Proceed to Checkout
+                  </button>
+                </form>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </section>
